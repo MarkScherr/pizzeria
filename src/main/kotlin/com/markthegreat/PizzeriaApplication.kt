da4603b29ac157f8476424c95d5a87de3dda5410
MarkScherr <mrscherr@gmail.com>
2023-04-16 15:37:20 -0500

    initial commit

---
+++ src/main/kotlin/com/markthegreat/PizzeriaApplication.kt
@@ -0,0 +1,11 @@
+package com.markthegreat.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PizzeriaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PizzeriaApplication>(*args)
+}
